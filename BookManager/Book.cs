e242fb277aef23d51b6b9588977d73f45956357e
Jorge Monraz <JMonraz@student.fullsail.edu>
2022-11-07 12:48:13 -0800

    Add project files.

---
+++ BookManager/Book.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BookManager
+{
+    public class Book
+    {
+        protected string _title = "Title: ";
+        protected string _author = "Author: ";
+        protected string _pages = "Pages: ";
+        protected string _genre = "Genre: ";
+
+        protected string _file = @".\books.txt";
+
+        protected List<Book> _textLines = new List<Book>();
+
+        void AddBooks()
+        {
+            Book book = new Book();
+            if (File.Exists(_file))
+            {
+                StreamReader textFile = new StreamReader(_file);
+
+                string currLine;
+
+                if (_textLines.Count == 0)
+                {
+                    while ((currLine = textFile.ReadLine()) != null)
+                    {
+                        if (currLine.StartsWith(_title))
+                        {
+                            book._title = currLine;
+                        }
+                        if (currLine.StartsWith(_author))
+                        {
+                            book._author = currLine;
+                        }
+                        if (currLine.StartsWith(_pages))
+                        {
+                            book._pages = currLine;
+                        }
+                        if (currLine.StartsWith(_genre))
+                        {
+                            book._genre = currLine;
+                            _textLines.Add(new Book { _title = book._title, _author = book._author, _pages = book._pages, _genre = book._genre });
+                        }
+                    }
+                }
+                textFile.Close();
+            }
+        }
+        public void CreateBook()
+        {
+            Book book = new Book();
+            Book tempBook = new Book();
+
+            Console.Write("Enter book title: ");
+            tempBook._title = "Title: " + Console.ReadLine();
+
+            Console.Write("Enter author name: ");
+            tempBook._author = "Author: " + Console.ReadLine();
+
+            Console.Write("Enter number of pages: ");
+            tempBook._pages = "Pages: " + Console.ReadLine();
+
+            Console.Write("Enter book genre: ");
+            tempBook._genre = "Genre: " + Console.ReadLine();
+
+            bool bookExists = false;
+
+            if (File.Exists(_file))
+            {
+                StreamReader textFile = new StreamReader(_file);
+
+                string currLine;
+
+                if (_textLines.Count == 0)
+                {
+                    while ((currLine = textFile.ReadLine()) != null)
+                    {
+                        if (currLine.StartsWith(_title))
+                        {
+                            book._title = currLine;
+                        }
+                        if (currLine.StartsWith(_author))
+                        {
+                            book._author = currLine;
+                        }
+                        if (currLine.StartsWith(_pages))
+                        {
+                            book._pages = currLine;
+                        }
+                        if (currLine.StartsWith(_genre))
+                        {
+                            book._genre = currLine;
+                            _textLines.Add(new Book { _title = book._title, _author = book._author, _pages = book._pages, _genre = book._genre });
+                        }
+
+                    }
+                }
+
+                for (int i = 0; i < _textLines.Count; i++)
+                {
+                    if (_textLines[i]._title == tempBook._title)
+                    {
+                        bookExists = true;
+                        Console.WriteLine();
+                        Console.WriteLine($"Book with {_textLines[i]._title} already exists");
+                        Console.WriteLine("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+                    }
+                }
+                textFile.Close();
+            }
+            if (!bookExists)
+            {
+
+                using (StreamWriter w = File.AppendText(_file))
+                {
+                    _textLines.Add(tempBook);
+                    for (int i = 0; i < 1; i++)
+                    {
+                        w.WriteLine(tempBook._title);
+                        w.WriteLine(tempBook._author);
+                        w.WriteLine(tempBook._pages);
+                        w.WriteLine(tempBook._genre);
+                    }
+
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Book saved successfully");
+                Console.WriteLine("Enter any key to continue");
+                Console.ReadKey();
+            }
+
+
+        }
+
+        public void DisplayBooks()
+        {
+            AddBooks();
+
+            if(_textLines.Count == 0)
+            {
+                Console.WriteLine("There are no books in your library");
+            }
+            else
+            {
+                for(int i = 0; i < _textLines.Count; i++)
+                {
+                    Console.WriteLine("--------------------------------------------------------------");
+                    Console.WriteLine(_textLines[i]._title);
+                    Console.WriteLine(_textLines[i]._author);
+                    Console.WriteLine(_textLines[i]._pages);
+                    Console.WriteLine(_textLines[i]._genre);
+                }
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nPress any key to continue");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+
+        public void EraseBook()
+        {
+
+            AddBooks();
+
+            bool isTrue = false;
+            while(!isTrue)
+            {
+                Console.Clear();
+                int bookCount = 0;
+
+                for (int i = 0; i < _textLines.Count; i++)
+                {
+                    Console.WriteLine((i + 1) + ") " + _textLines[i]._title);
+                    bookCount++;
+                }
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Press {bookCount + 1} to go BACK");
+                Console.ResetColor();
+                Console.WriteLine();
+                Console.Write("Which book do you want to delete?: ");
+                string value = Console.ReadLine();
+                int option = 0;
+
+                if (!int.TryParse(value, out option))
+                {
+                    Console.WriteLine("Invalid input");
+                    Console.ReadKey();
+                }
+                else if (int.TryParse(value, out option) && option <= 0 || option > bookCount && option != bookCount + 1)
+                {
+                    Console.WriteLine("Invalid input");
+                    Console.ReadKey();
+                }
+                else if (int.TryParse(value, out option) && option == bookCount + 1)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"{_textLines[option - 1]._title} was removed.");
+                    Console.WriteLine("Press any KEY to continue");
+                    Console.ReadKey();
+                    _textLines.Remove(_textLines[option - 1]);
+                    StreamWriter w = new StreamWriter(_file);
+                    w.Flush();
+                    w.Close();
+
+                    
+                    using (w = File.AppendText(_file))
+                    {
+                        for (int i = 0; i < _textLines.Count; i++)
+                        {
+                            w.WriteLine(_textLines[i]._title);
+                            w.WriteLine(_textLines[i]._author);
+                            w.WriteLine(_textLines[i]._pages);
+                            w.WriteLine(_textLines[i]._genre);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void WriteReview()
+        {
+
+            AddBooks();
+
+            bool valid = false;
+
+            while(!valid)
+            {
+                Console.Clear();
+                int bookCount = 0;
+
+                for (int i = 0; i < _textLines.Count; i++)
+                {
+                    Console.WriteLine((i+1) + ")" + _textLines[i]._title);
+                    bookCount++;
+                    
+                }
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Press " + (bookCount+1) +" to go BACK");
+                Console.ResetColor();
+                Console.WriteLine();
+                Console.Write("For which book do you want to write a review?: ");
+
+                string value = Console.ReadLine();
+                int option = 0;
+
+                if (!int.TryParse(value, out option))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Not a valid option");
+                    Console.ResetColor();
+                    Console.ReadKey();
+                }
+                    
+                else if (int.TryParse(value, out option) && option <= 0 || option > bookCount && option != bookCount+1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Not a valid option");
+                    Console.ResetColor();
+                    Console.ReadKey();
+
+                }
+                else if(int.TryParse(value, out option) && option == bookCount+1)
+                {
+                    break;
+                }
+                else
+                {
+                    string str = _textLines[option - 1]._title;
+                    string newstr = str.Remove(0, 7);
+                    string currFile = @".\" + newstr + ".txt";
+                    Console.Clear();
+                    Console.Write("Write your review for " + newstr + ", press any ENTER when you are done: ");
+                    string review = Console.ReadLine();
+                    using (StreamWriter w = File.AppendText(currFile))
+                    {
+                        w.Write(review);
+                    }
+
+                    Console.WriteLine("Review saved. Press any key to continue.");
+                    Console.ReadKey();
+                }
+            }
+            
+        }
+
+        public void EditBook()
+        {
+            AddBooks();
+
+            bool valid = false;
+
+            while (!valid)
+            {
+                Console.Clear();
+                int bookCount = 0;
+
+                for (int i = 0; i < _textLines.Count; i++)
+                {
+                    Console.WriteLine((i + 1) + ")" + _textLines[i]._title);
+                    bookCount++;
+
+                }
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Press " + (bookCount + 1) + " to go BACK");
+                Console.ResetColor();
+                Console.WriteLine();
+                Console.Write("Which book do you want to edit? ");
+
+                string value = Console.ReadLine();
+                int option = 0;
+
+                if (!int.TryParse(value, out option))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Not a valid option");
+                    Console.ResetColor();
+                    Console.ReadKey();
+                }
+
+                else if (int.TryParse(value, out option) && option <= 0 || option > bookCount && option != bookCount + 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Not a valid option");
+                    Console.ResetColor();
+                    Console.ReadKey();
+
+                }
+                else if (int.TryParse(value, out option) && option == bookCount + 1)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Update book");
+                    _textLines.Remove(_textLines[option - 1]);
+                    StreamWriter w = new StreamWriter(_file);
+                    w.Flush();
+                    w.Close();
+
+                    using (w = File.AppendText(_file))
+                    {
+                        for (int i = 0; i < _textLines.Count; i++)
+                        {
+                            w.WriteLine(_textLines[i]._title);
+                            w.WriteLine(_textLines[i]._author);
+                            w.WriteLine(_textLines[i]._pages);
+                            w.WriteLine(_textLines[i]._genre);
+                        }
+                    }
+
+                    CreateBook();
+                }
+            }
+        }
+    }
+}
---
+++ BookManager/Program.cs
@@ -0,0 +1,93 @@
+﻿using BookManager;
+
+namespace BookManager
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+
+            bool isOn = true;
+
+            while(isOn)
+            {
+                Console.Clear();
+                Console.WriteLine("\t\t\t==============================================");
+                Console.WriteLine("\t\t\t========== Book Manager      =================");
+                Console.WriteLine("\t\t\t==========      Menu         =================");
+                Console.WriteLine("\t\t\t==============================================");
+
+                Console.WriteLine("\n");
+                Console.WriteLine("\t\t\t\t----------------------------------");
+                Console.WriteLine("\t\t\t\t| 1) Create book                 |");
+                Console.WriteLine("\t\t\t\t| 2) Edit book                   |");
+                Console.WriteLine("\t\t\t\t| 3) Erase book                  |");
+                Console.WriteLine("\t\t\t\t| 4) See Progress by book        |");
+                Console.WriteLine("\t\t\t\t| 5) Display library             |");
+                Console.WriteLine("\t\t\t\t| 6) Search book                 |");
+                Console.WriteLine("\t\t\t\t| 7) Create Collection           |");
+                Console.WriteLine("\t\t\t\t| 8) Write a review              |");
+                Console.WriteLine("\t\t\t\t| 9) About                       |");
+                Console.WriteLine("\t\t\t\t| 10) Exit                       |");
+                Console.WriteLine("\t\t\t\t----------------------------------");
+
+                Console.WriteLine();
+                Console.Write("\t\t\t\tInput option: ");
+
+                string value = Console.ReadLine();
+                int input = 0;
+
+                if(!int.TryParse(value, out input) && input <= 0 || input > 10)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Invalid input");
+                    Console.ReadKey();
+                }
+
+                else if (input == 1)
+                {
+                    book.CreateBook();
+                }
+                else if (input == 2)
+                {
+                    book.EditBook();
+                }
+                else if(input == 3)
+                {
+                    book.EraseBook();
+                }
+                else if (input == 5)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\n\n\n\n\n\n\n\t\t\t\t| 1) Display by Title            |");
+                    Console.WriteLine("\t\t\t\t| 2) Display by Author           |");
+                    Console.WriteLine("\t\t\t\t| 3) Display by Numbers of Pages |");
+                    Console.WriteLine("\t\t\t\t| 4) Display by Genre            |");
+                    Console.ReadKey();
+                    Console.Clear();
+                    book.DisplayBooks();
+                }
+                else if(input ==8)
+                {
+                    book.WriteReview();
+                }
+                else if (input == 9)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("\n\n");
+                    Console.WriteLine("\t\t\t\t Program under development");
+                    Console.WriteLine("\t\t\t\t Property of Jorge Monraz");
+                    Console.WriteLine("\t\t\t\t Last date modified: 11/07/2022");
+                    Console.ResetColor();
+                    Console.ReadKey();
+                }
+                else if (input == 10)
+                {
+                    isOn = false;
+                }
+            }
+        }
+    }
+}
